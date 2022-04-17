a0b47be04687481c9951cf1b26aedacaaae94368
Phong Nguyen <nhatphongcgp@gmail.com>
2022-04-17 21:20:08 +0700

    fix bug

--- server.js
+++ server.js
@@ -1,47 +1,52 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
 const { admin } = require('./firebase-config');
-const app = express();
-
-app.use(bodyParser.json())
-
-const notification_options = {
-    priority: "high",
-    timeToLive: 60 * 60 * 24
-};
-//pass token here from client;
-const registrationToken = "token"
+var registrationToken = "cmID6fJBRaiqK5yfuvsEWl:APA91bEKd_svYJNnOF4im0CQPO1lnZE5SgHd4Qf-aqVdRJ-WOcfJ42ReGK6ff4GG9Vc3zzazJKM_uBJTgSSLxSVn_hR5rZ_eDZ9Jdnu3vP1vT7UhHbTZK8evaBQ_tdHAhm6Enc-Quyb1";
 
 var payload = {
     notification: {
-        title: "This is a Notification",
-        body: "This is the body of the notification message."
+        title: "Account Deposit",
+        body: "A deposit to your savings account has just cleared."
+    },
+    data: {
+        account: "Savings",
+        balance: "$3020.25"
     }
+};
+
+var options = {
+    priority: "normal",
+    timeToLive: 60 * 60
 };
 
 
 var db = admin.database();
-
-var description = "default";
-
-var ref = db.ref("User").on("value", function (snapshot) {
-    description = snapshot.toJSON().city;
+var ref = db.ref("User").child("address").on("value", function name(snsh) {
 
     payload = {
         notification: {
-            title:  snapshot.toJSON().state,
-            body: description,
+            title: "Account Deposit",
+            body: snsh.val().state,
+        },
+        data: {
+            account: "Savings",
+            balance: "$3020.25"
         }
     };
+    console.log(payload);
 
-    admin.messaging().sendToDevice( registrationToken, payload, notification_options)
+    admin.messaging().sendToDevice(registrationToken, payload, options)
         .then(function (response) {
             console.log("Successfully sent message:", response);
         })
         .catch(function (error) {
             console.log("Error sending message:", error);
         });
-});
 
 
+})
 console.log(ref);
+
+
+
+
