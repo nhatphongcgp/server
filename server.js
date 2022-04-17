830f5b02c642ae203cfb1f102d1363931d659df8
Phong Nguyen <nhatphongcgp@gmail.com>
2022-04-17 20:30:45 +0700

    done

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const { admin } = require('./firebase-config');
-
 const app = express();
 
 app.use(bodyParser.json())
@@ -10,10 +9,9 @@ const notification_options = {
     priority: "high",
     timeToLive: 60 * 60 * 24
 };
+//pass token here from client;
+const registrationToken = "token"
 
-
-
-const registrationToken = "fl3Rv7XzQ72Oxie2CYBx1C:APA91bEhDPQTC4rnqTpDGzxiXbIIXTX77bECpX6cyr9EpzgXaSr1H_njrC6G74Li94AK9kOPVbzoQbZ4Tr3oBJXMvmbWYmiI6KwVP3H3JEbbS62JXzQWCKS2Hv_r19QTP9QIz-BeAXZ_"
 var payload = {
     notification: {
         title: "This is a Notification",
@@ -21,10 +19,29 @@ var payload = {
     }
 };
 
-admin.messaging().sendToDevice(registrationToken, payload, notification_options)
-    .then(function (response) {
-        console.log("Successfully sent message:", response);
-    })
-    .catch(function (error) {
-        console.log("Error sending message:", error);
-    });
+
+var db = admin.database();
+
+var description = "default";
+
+var ref = db.ref("User").on("value", function (snapshot) {
+    description = snapshot.toJSON().city;
+
+    payload = {
+        notification: {
+            title:  snapshot.toJSON().state,
+            body: description,
+        }
+    };
+
+    admin.messaging().sendToDevice( registrationToken, payload, notification_options)
+        .then(function (response) {
+            console.log("Successfully sent message:", response);
+        })
+        .catch(function (error) {
+            console.log("Error sending message:", error);
+        });
+});
+
+
+console.log(ref);
